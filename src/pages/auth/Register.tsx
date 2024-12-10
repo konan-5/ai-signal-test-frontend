50d158f46e17035d96ed98de6cb87cda6ce296df
konan-5 <ikedahiroshi517@gmail.com>
2024-12-11 02:18:22 +0900

    feat: Add register page

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,4 +1,41 @@
+import { useState } from "react";
+import Button from "../../components/common/Button";
+import FormInput from "../../components/common/FormInput";
+import { Link } from "react-router-dom";
 export default function LoginPage() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleSubmit = () => {
+        // Reset errors
+        const newErrors = {
+            email: "",
+            password: ""
+        };
+
+        if (!email) {
+            newErrors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            newErrors.email = "Invalid email format";
+        }
+
+        if (!password) {
+            newErrors.password = "Password is required";
+        } else if (password.length < 6) {
+            newErrors.password = "Password must be at least 6 characters";
+        }
+
+        setErrors(newErrors);
+
+        if (!newErrors.email && !newErrors.password) {
+            console.log("Logging in...");
+        }
+    };
 
     return (
         <div className="container px-4 mx-auto h-full flex items-center justify-center">
@@ -6,6 +43,24 @@ export default function LoginPage() {
                 <p className="text-white font-open-sans text-3xl font-bold">
                     Register
                 </p>
+                <FormInput
+                    title="Email"
+                    type="email"
+                    value={email}
+                    onChange={(value) => setEmail(value)}
+                    placeholder="Enter your email"
+                    error={errors.email}
+                />
+                <FormInput
+                    title="Password"
+                    type="password"
+                    value={password}
+                    onChange={(value) => setPassword(value)}
+                    placeholder="Enter your password"
+                    error={errors.password}
+                />
+                <p className="text-gray-light text-sm font-open-sans">Already have an account? <Link to="/login" className="text-secondary underline">Login</Link></p>
+                <Button title="Register" variant="secondary" onClick={handleSubmit} />
             </div>
         </div>
     )
